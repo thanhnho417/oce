97bbfe0ffbd82d7950433252e788c9f6796428f8
thanhnho417 <trieudoquoc417@outlook.com>
2024-07-04 23:06:02 +0700

    update for presentation and home

--- pages/project/presentation/script.js
+++ pages/project/presentation/script.js
@@ -6,9 +6,18 @@ async function loadTableData() {
         let tableBody = document.querySelector('#dataTable tbody');
         tableBody.innerHTML = '';
 
-        data.forEach(item => {
+        data.forEach((item, index) => {
             let row = document.createElement('tr');
-            row.innerHTML = `<td>${item.id}</td><td>${item.name}</td><td>${item.info}</td><td>${item.type}</td><td><img src="${item.access}"></td>`;
+            row.innerHTML = `
+                <td>${index + 1}</td>
+                <td>${item.name}</td>
+                <td>${item.info}</td>
+                <td>${item.type}</td>
+                <td>
+                    <a href="${item.downloadlink}" target="_blank"><img src="${item.download}" alt="Download"></a>
+                    <a href="${item.accesslink}" target="_blank"><img src="${item.access}" alt="Access"></a>
+                </td>
+            `;
             tableBody.appendChild(row);
         });
     } catch (error) {
