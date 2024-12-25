16ad84eaf0fcbca13da18aaacf676574db14cac7
thanhnho417 <trieudoquoc417@outlook.com>
2024-12-25 21:27:19 +0700

    up

--- javascript/img-gal.js
+++ javascript/img-gal.js
@@ -4,8 +4,8 @@ imagesLoaded(grid, function () {
     new Masonry(grid, {
         itemSelector: '.me-pho',
         columnWidth: '.me-pho',
-        percentPosition: true, // Đảm bảo tính toán theo phần trăm
-        gutter: 10, // Khoảng cách giữa các phần tử
+        percentPosition: true,
+        gutter: 10,
     });
 
     grid.classList.add('loaded');
