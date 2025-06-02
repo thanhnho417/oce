236a34557fc3d3bbdc472986157b51444e0af36c
thanhnho417 <trieudoquoc417@outlook.com>
2025-06-02 22:53:16 +0700

    up

--- sample/script.js
+++ sample/script.js
@@ -1,193 +1,80 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const player = new Plyr('#player', { captions: { active: true, language: 'vi' } });
-    const video = document.querySelector('#player');
-    const titleElement = document.getElementById('title');
-    const descriptionElement = document.getElementById('description');
-    const thumbnailElement = document.querySelector('.thumbnail');
-    let hls;
-
-    function loadVideo(sourceUrl) {
-        if (Hls.isSupported()) {
-            if (hls) {
-                hls.destroy();
-            }
-            hls = new Hls();
-            hls.loadSource(sourceUrl);
-            hls.attachMedia(video);
-        } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-            video.src = sourceUrl;
-        } else {
-            console.error('HLS is not supported in this browser.');
-        }
-    }
-
-    function addSubtitleTracks(subtitles) {
-        console.log('Adding subtitles:', subtitles);  // Kiểm tra dữ liệu phụ đề
-        const existingTracks = video.querySelectorAll('track');
-        existingTracks.forEach(track => track.remove());  // Loại bỏ các track cũ trước khi thêm mới
-    
-        subtitles.forEach(subtitle => {
-            // Kiểm tra nếu src của phụ đề tồn tại
-            if (subtitle.src) {
-                const track = document.createElement('track');
-                track.kind = 'subtitles';
-                track.label = subtitle.label || 'Subtitles';
-                track.src = subtitle.src;
-                track.srclang = subtitle.srclang || 'en';
-                track.default = subtitle.default || false;
-    
-                // Đảm bảo video đã tải và có thể thêm track vào
-                if (video.readyState >= 3) {
-                    video.appendChild(track);
-                } else {
-                    video.addEventListener('loadedmetadata', () => {
-                        video.appendChild(track);
-                    });
-                }
-            }
-        });
-    }
-    
-
-    function updateVideoInfo(data) {
-        if (data.title) {
-            titleElement.textContent = data.title;
-        }
-        if (data.src) {
-            loadVideo(data.src);
-        }
-        if (data.description) {
-            descriptionElement.innerHTML = data.description;
-        }
-        if (data.thumbnail) {
-            thumbnailElement.src = data.thumbnail;
-        }
-        if (data.subtitles) {
-            addSubtitleTracks(data.subtitles); // Thêm phụ đề từ data JSON
-        }
-    }
-    
-
-    function initializeToggleButton(toggleBtnId, descriptionId, maxLines = 3) {
-        const toggleButton = document.getElementById(toggleBtnId);
-        const descriptionElement = document.getElementById(descriptionId);
-
-        if (!toggleButton || !descriptionElement) {
-            console.error('Không tìm thấy nút hoặc phần mô tả.');
-            return;
-        }
-
-        let isExpanded = false;
-
-        function updateDescription() {
-            if (isExpanded) {
-                descriptionElement.style.display = 'block';
-                toggleButton.textContent = 'Ẩn bớt';
-            } else {
-                descriptionElement.style.display = '-webkit-box';
-                descriptionElement.style.webkitBoxOrient = 'vertical';
-                descriptionElement.style.overflow = 'hidden';
-                descriptionElement.style.webkitLineClamp = maxLines;
-                toggleButton.textContent = 'Hiện thêm';
-            }
-        }
-
-        toggleButton.addEventListener('click', () => {
-            isExpanded = !isExpanded;
-            updateDescription();
-        });
-
-        updateDescription();
-    }
-
-    function updatePlaylist(data) {
-        const playlistContainer = document.querySelector('.playlist');
-        playlistContainer.innerHTML = '';
-
-        data.forEach(videoData => {
-            const playlistItem = document.createElement('div');
-            playlistItem.classList.add('playlist-video');
-            playlistItem.setAttribute('data-src', videoData.src);
-
-            const imgElement = document.createElement('img');
-            imgElement.setAttribute('src', videoData.thumbnail);
-            imgElement.setAttribute('alt', 'Thumbnail');
-            playlistItem.appendChild(imgElement);
-
-            const titleElement = document.createElement('p');
-            titleElement.classList.add('title-pl');
-            titleElement.textContent = videoData.title;
-            playlistItem.appendChild(titleElement);
-
-            const descriptionElement = document.createElement('p');
-            descriptionElement.classList.add('description-pl');
-            descriptionElement.style.display = 'none';
-            descriptionElement.innerHTML = videoData.description;
-            playlistItem.appendChild(descriptionElement);
-
+window.availableKeywords = []; // Biến toàn cục để lưu trữ từ khóa tìm kiếm
+function searchbtt() {
+        const searchInput = document.getElementById('search-input');
+        const closebtt = document.querySelector('.close-button');
+        const searchButton = document.querySelector('.search-button');
+        const grayScreen = document.querySelector('.gray-screen');
+        const searchPlace = document.querySelector('.search-place');
+        searchPlace.style.display = 'block'; // Hiển thị danh sách tìm kiếm
+        searchPlace.style.opacity = '1'; // Đảm bảo danh sách tìm kiếm hiển thị
+        searchPlace.style.transition = 'opacity 0.3s ease'; // Thêm hiệu ứng chuyển tiếp
+        searchInput.classList.add('active-input');
+        searchInput.focus();
+        closebtt.style.display = 'flex';
+        searchButton.style.display = 'none';
+        grayScreen.classList.add('active-gray-screen');
+      }
+      
+      function closeSearch() {
+        const searchInput = document.getElementById('search-input');
+        const closebtt = document.querySelector('.close-button');
+        const searchButton = document.querySelector('.search-button');
+        const grayScreen = document.querySelector('.gray-screen');
+        const searchPlace = document.querySelector('.search-place');
+        searchPlace.style.display = 'none'; // Ẩn danh sách tìm kiếm
+        searchInput.classList.remove('active-input');
+        closebtt.style.display = 'none';
+        searchButton.style.display = 'flex';
+        grayScreen.classList.remove('active-gray-screen');
+      }
+      // Đóng tìm kiếm khi click ra ngoài
+      document.addEventListener('click', function(event) {
+        const searchForm = document.querySelector('.searchform');
+        const searchInput = document.getElementById('search-input');
+        const searchplace = document.querySelector('.search-place');
         
-
-            playlistContainer.appendChild(playlistItem);
-
-            playlistItem.addEventListener('click', () => {
-                updateVideoInfo(videoData);
+        if (!searchForm.contains(event.target) && !searchplace.contains(event.target) && searchInput.classList.contains('active-input')) {
+          closeSearch();
+        }
+      });
+function searchengine(){
+    const resultbox = document.querySelector('.search-place');
+    const inputbox = document.getElementById('search-input');
+    inputbox.onkeyup = function(){
+        const input = inputbox.value;
+        if (input.length){
+            const key = window.availableKeywords.filter((keyword) => {
+                return keyword.toLowerCase().includes(input.toLowerCase());
             });
-        });
-    }
-
-    function loadPlaylistData() {
-        fetch('videos.json')
-            .then(response => response.json())
-            .then(data => {
-                if (data && data.length > 0) {
-                    // Kiểm tra và xử lý nếu video không có phụ đề
-                    data.forEach(videoData => {
-                        if (videoData.subtitles && videoData.subtitles.length > 0) {
-                            // Kiểm tra sự tồn tại của tệp phụ đề
-                            videoData.subtitles.forEach(subtitle => {
-                                fetch(subtitle.src)
-                                    .then(subtitleResponse => {
-                                        if (subtitleResponse.ok) {
-                                            // Nếu tệp phụ đề tồn tại, thêm vào video
-                                            updatePlaylist(data); // Cập nhật playlist
-                                            updateVideoInfo(data[0]); // Cập nhật video đầu tiên
-                                        } else {
-                                            console.warn(`Không tìm thấy phụ đề tại ${subtitle.src}`);
-                                        }
-                                    })
-                                    .catch(err => {
-                                        console.error(`Lỗi khi tải phụ đề: ${subtitle.src}`, err);
-                                    });
-                            });
-                        } else {
-                            console.warn("Video không có phụ đề.");
-                            updatePlaylist(data); // Cập nhật playlist mà không có phụ đề
-                            updateVideoInfo(data[0]); // Cập nhật video đầu tiên
-                        }
-                    });
-                }
-            })
-            .catch(error => {
-                console.error('Không thể tải dữ liệu video:', error);
+            const createul = document.createElement('ul');
+            key.forEach((item) => {
+                const createli = document.createElement('li');
+                createli.textContent = item;
+                createul.appendChild(createli);
             });
-    }
-    
-
-    function adjustPlaylistHeight() {
-        const main = document.querySelector('.main');
-        const playlist = document.querySelector('.playlist');
-
-        if (main && playlist) {
-            const mainHeight = main.offsetHeight;
-            playlist.style.height = `${mainHeight}px`;
+            resultbox.innerHTML = ''; // Xóa nội dung cũ
+            resultbox.appendChild(createul);
+        } else {
+           
+            resultbox.innerHTML = '';
+            resultbox.textContent = 'Tìm tại đây'
         }
     }
+}
 
-    window.addEventListener('load', () => {
-        loadPlaylistData();
-        adjustPlaylistHeight();
-        initializeToggleButton('toggle-btn', 'description', 3);
-    });
 
-    window.addEventListener('resize', adjustPlaylistHeight);
-});
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+        fetch('https://oce.pages.dev/results/data.json')
+          .then(Response => Response.json())
+          .then(data => {
+            const searchPlace = document.querySelector('.search-place');
+            const createul = document.createElement('ul');
+            searchPlace.innerHTML = ''; // Xóa nội dung cũ
+            window.availableKeywords = data.map(item => item.category + " | " + item.description + " | " +  item.title);
+            
+            searchengine(); // Gọi hàm tìm kiếm sau khi dữ liệu đã được tải
+          })
+      })
